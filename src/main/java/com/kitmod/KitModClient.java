aae5c38cb3b601f975d5bc570b468111fd0cf2ec
dbg7830-dotcom <dbg7830@gmail.com>
2026-08-07 16:45:44 +0530

    Update KitModClient.java

--- src/main/java/com/kitmod/KitModClient.java
+++ src/main/java/com/kitmod/KitModClient.java
@@ -10,8 +10,10 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenKeyboardEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenMouseEvents;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.input.KeyInput;
+import net.minecraft.client.input.MouseInput;
 
 public class KitModClient implements ClientModInitializer {
 
@@ -20,47 +22,57 @@ public class KitModClient implements ClientModInitializer {
         KitKeybind.register();
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (KitKeybind.OPEN_GUI.consumeClick()) {
-                if (client.screen == null) {
-                    Minecraft.getInstance().setScreen(new KitManagerScreen());
+            while (KitKeybind.OPEN_GUI.wasPressed()) {
+                if (client.currentScreen == null) {
+                    MinecraftClient.getInstance().setScreen(new KitManagerScreen());
                 }
             }
         });
 
         ScreenEvents.AFTER_INIT.register(new ScreenEvents.AfterInit() {
             @Override
-            public void afterInit(Minecraft client, Screen screen, int scaledWidth, int scaledHeight) {
+            public void afterInit(MinecraftClient client, Screen screen, int scaledWidth, int scaledHeight) {
+
                 if (screen instanceof KitManagerScreen kitScreen) {
                     ScreenMouseEvents.beforeMouseClick(screen).register(
                         new ScreenMouseEvents.BeforeMouseClick() {
                             @Override
-                            public void beforeMouseClick(Screen s, double mx, double my, int btn) {
-                                kitScreen.handleClick(mx, my, btn);
+                            public void beforeMouseClick(Screen s, MouseInput event) {
+                                // Coordinates come from the mouse object, not the event
+                                double mx = client.mouse.getX() * scaledWidth / client.getWindow().getWidth();
+                                double my = client.mouse.getY() * scaledHeight / client.getWindow().getHeight();
+                                kitScreen.handleClick(mx, my, event.button());
                             }
                         }
                     );
                     ScreenKeyboardEvents.beforeKeyPress(screen).register(
                         new ScreenKeyboardEvents.BeforeKeyPress() {
                             @Override
-                            public void beforeKeyPress(Screen s, int key, int scan, int mods) {
-                                kitScreen.handleKey(key);
+                            public void beforeKeyPress(Screen s, KeyInput event) {
+                                kitScreen.handleKey(event.key());
                             }
                         }
                     );
+
                 } else if (screen instanceof MarketplaceScreen mScreen) {
                     ScreenMouseEvents.beforeMouseClick(screen).register(
                         new ScreenMouseEvents.BeforeMouseClick() {
                             @Override
-                            public void beforeMouseClick(Screen s, double mx, double my, int btn) {
+                            public void beforeMouseClick(Screen s, MouseInput event) {
+                                double mx = client.mouse.getX() * scaledWidth / client.getWindow().getWidth();
+                                double my = client.mouse.getY() * scaledHeight / client.getWindow().getHeight();
                                 mScreen.handleClick(mx, my);
                             }
                         }
                     );
+
                 } else if (screen instanceof MarketplaceKitDetailScreen dScreen) {
                     ScreenMouseEvents.beforeMouseClick(screen).register(
                         new ScreenMouseEvents.BeforeMouseClick() {
                             @Override
-                            public void beforeMouseClick(Screen s, double mx, double my, int btn) {
+                            public void beforeMouseClick(Screen s, MouseInput event) {
+                                double mx = client.mouse.getX() * scaledWidth / client.getWindow().getWidth();
+                                double my = client.mouse.getY() * scaledHeight / client.getWindow().getHeight();
                                 dScreen.handleClick(mx, my);
                             }
                         }
@@ -68,16 +80,19 @@ public class KitModClient implements ClientModInitializer {
                     ScreenKeyboardEvents.beforeKeyPress(screen).register(
                         new ScreenKeyboardEvents.BeforeKeyPress() {
                             @Override
-                            public void beforeKeyPress(Screen s, int key, int scan, int mods) {
-                                dScreen.handleKey(key);
+                            public void beforeKeyPress(Screen s, KeyInput event) {
+                                dScreen.handleKey(event.key());
                             }
                         }
                     );
+
                 } else if (screen instanceof MarketplaceUploadScreen uScreen) {
                     ScreenMouseEvents.beforeMouseClick(screen).register(
                         new ScreenMouseEvents.BeforeMouseClick() {
                             @Override
-                            public void beforeMouseClick(Screen s, double mx, double my, int btn) {
+                            public void beforeMouseClick(Screen s, MouseInput event) {
+                                double mx = client.mouse.getX() * scaledWidth / client.getWindow().getWidth();
+                                double my = client.mouse.getY() * scaledHeight / client.getWindow().getHeight();
                                 uScreen.handleClick(mx, my);
                             }
                         }
@@ -85,8 +100,8 @@ public class KitModClient implements ClientModInitializer {
                     ScreenKeyboardEvents.beforeKeyPress(screen).register(
                         new ScreenKeyboardEvents.BeforeKeyPress() {
                             @Override
-                            public void beforeKeyPress(Screen s, int key, int scan, int mods) {
-                                uScreen.handleKey(key);
+                            public void beforeKeyPress(Screen s, KeyInput event) {
+                                uScreen.handleKey(event.key());
                             }
                         }
                     );
